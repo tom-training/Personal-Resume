ef323de8893a6e17a0aba6e44da106f9b26b17da
tomogator <guethomas@hotmail.com>
2021-03-16 19:22:34 +0100

    first commit

---
+++ contact.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+		<meta charset = "utf-8"/>
+
+		<link rel="stylesheet" media="screen and (min-width: 1025px)" href="style1.css"/>  
+
+		<link rel="stylesheet" media="screen and (max-width: 1024px)" href="petite_resolution.css"/>
+
+		<title> TGCV </title>
+
+</head>
+
+
+
+<body id="contact">
+
+	
+
+	
+
+		
+<?php include("menu.php"); ?>
+
+				
+
+	
+
+	<section id="containerContact">
+
+		<div id="containerGaucheContact">
+
+	    
+        <article class="artContact">
+        
+            <h1 class="contactMe"> Contact me </h1>
+
+            <div class="plsContact">
+            
+            	<p> Should you have any enquiries, please let me know and I will get back to you as soon as possible</p>
+                
+                <p>
+            	Flottwellstrasse 21<br>   
+				10785 Berlin<br>  
+				Germany <br>  
+				+49 (0) 15 20 400 8767<br> 
+         
+                </p>
+                
+          				
+			    <p>
+                <a href="mailto:contact@thomasgue.info" id="email"> contact@thomasgue.info </a> 
+              
+                </p>
+         
+                
+
+            </div>    
+
+	        
+
+
+            
+        </article>
+
+    	</div>
+
+    	<div id="containerDroitContact">
+
+    		<figure class="grandePix">
+				
+			<img src="images/mobile.jpg" alt="mobile" title="communication tools">
+
+			</figure>
+
+			
+
+
+
+    	</div>	
+
+
+	</section>
+    
+    
+
+
+<script type="text/javascript" src="checks.js"></script> 
+		
+
+</body>
+
+</html>
